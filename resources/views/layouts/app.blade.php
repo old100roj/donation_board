01174a33f4e7c053a6ae9935fdde8892dfa42bd3
Dmitry <mdmytro@student.unit.ua>
2019-07-12 15:28:42 +0300

    gitignore + ide helper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,20 +23,32 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="{{ route('donates.index') }}">{{__('Donations')}}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <form>
+        <div class="form-row">
+            <a class="navbar-brand" href="{{ route('donates.index') }}">{{__('Donations')}}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('donates.create') }}">{{__('Make Donation')}}</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('donates.create') }}">{{__('Make Donation')}}</a>
+                    </li>
+                </ul>
+                <form class="form-inline ml-auto">
+                    <div class="md-form my-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                        <input class="form-control mr-sm-2" type="number">
+                        <input class="form-control mr-sm-2" type="number">
+                        <input class="form-control mr-sm-2" type="date">
+                        <input class="form-control mr-sm-2" type="date">
+                        <button class="btn btn-mdb-color btn-rounded btn-sm my-0 ml-sm-2" type="submit">Search</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </nav>
+    </form>
 
     <div class="container-fluid">
         @yield('content')
